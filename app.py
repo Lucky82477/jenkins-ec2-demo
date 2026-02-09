fe7734af3d74fb010d114c93edccf7c616d37f32
Lucky82477 <sreelakshmikadapaka@gmail.com>
2026-02-09 21:11:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Jenkins Pipeline is WORKING on EC2 sucessfully 🚀"
+    return "Jenkins Pipeline is WORKING on EC2 sucessfully to new web page 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
